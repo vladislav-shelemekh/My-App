f9b4d1139719495250ca4f0ec407b308561bddac
Vladislav Shelemekh <nobaklazhan@gmail.comm>
2022-09-05 19:56:49 +0300

    _unfinished

--- index.js
+++ index.js
@@ -194,13 +194,28 @@ webix.ui({
             ]},
             { id:"Products", rows: [
               { view: "treetable",
-                cols: [
-                  { id:"id",	header:""},
-                  { id:"title",	header:"Title", template:"{common.treetable()} #title#" },
-                  { id:"price",	header:"Price"	}
+                columns: [
+                  { id:"id",	header:"", fillspace:true},
+                  { id:"title",	header:"Title", template:"{common.treetable()} #title#", fillspace:true },
+                  { id:"price",	header:"Price", fillspace:true	}
               ], 
               select:true,
               url: "products.js",
+              // data: [
+              //   { "id":"1", "title":"Cinema", "data":[
+              //     { "id":"1.1", "title":"Standard Ticket", "price": 21},
+              //     { "id":"1.2", "title":"Evening Ticket", "price": 27}
+              //   ]},
+              //   { "id":"2", "title":"Cafe", "data":[
+              //     { "id":"2.1", "title":"Cola", "price": 10},
+              //     { "id":"2.2", "title":"Mineral water", "price": 5},
+              //     { "id":"2.3", "title":"Pop Corn", "price": 7}
+              //   ]},
+              //   { "id":"3", "title":"Other", "data":[
+              //     { "id":"3.1", "title":"Flowers", "price": 10},
+              //     { "id":"3.2", "title":"Film CD", "price": 15}
+              //   ]}
+              // ]
               }
             ]},
             { id:"Admin", template:"Admin"}
@@ -275,7 +290,7 @@ function ascSort(){
 function descSort(){
   $$("userList").sort("#name#","desc","string");
     
-};
+}
 
 
 function highlter() {
